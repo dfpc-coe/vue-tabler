25a8687494c2983b92b02c8e668d31c9b7d0ef6a
ingalls <nick@ingalls.ca>
2022-11-27 07:12:44 -0700

    Add linting

--- lib.js
+++ lib.js
@@ -1,5 +1,11 @@
+import Err from './components/Err.vue'
 import Select from './components/Select.vue'
+import Modal from './components/Modal.vue'
+import Progress from './components/Progress.vue'
 
 export {
-    Select
+    Err,
+    Select,
+    Modal,
+    Progress
 }
