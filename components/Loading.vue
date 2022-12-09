783679bbfd85786d430b8d54c10e95652eeb30de
ingalls <nick@ingalls.ca>
2022-12-09 11:00:13 -0700

    Add Loading Component

---
+++ components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+<div class='w-full'>
+    <div class='d-flex justify-content-center my-4'>
+        <div class="spinner-border" role="status"></div>
+    </div>
+
+    <template v-if='desc && desc.length'>
+        <div class='d-flex justify-content-center my-4'>
+            <div class='' v-text='desc'></div>
+        </div>
+    </template>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Loading',
+    props: {
+        desc: {
+            type: String,
+            default: 'Loading...'
+        }
+    }
+}
+</script>
--- lib.js
+++ lib.js
@@ -3,10 +3,12 @@ import Select from './components/Select.vue'
 import Modal from './components/Modal.vue'
 import Progress from './components/Progress.vue'
 import Input from './components/Input.vue'
+import Loading from './components/Loading.vue'
 
 export {
     Err,
     Select,
+    Loading,
     Modal,
     Progress,
     Input
