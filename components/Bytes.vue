613f5607c9b108676bbbadee68631dfc65689f32
ingalls <nick@ingalls.ca>
2023-03-28 13:52:37 -0600

    Add TablerBytes

---
+++ components/Bytes.vue
@@ -0,0 +1,21 @@
+<template>
+<span v-text='display'/>
+</template>
+
+<script>
+export default {
+    name: 'Bytes',
+    props: {
+        bytes: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+        display: function() {
+             const i = this.bytes == 0 ? 0 : Math.floor(Math.log(this.bytes) / Math.log(1024));
+             return (this.bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+         }
+    }
+}
+</script>
--- lib.js
+++ lib.js
@@ -7,10 +7,12 @@ import TablerLoading from './components/Loading.vue'
 import TablerToggle from './components/Toggle.vue'
 import TablerInput from './components/Input.vue'
 import TablerBreadCrumb from './components/BreadCrumb.vue';
+import TablerBytes from './components/Bytes.vue';
 import TablerEpoch from './components/Epoch.vue';
 import TablerEpochRange from './components/EpochRange.vue';
 
 export {
+    TablerBytes,
     TablerEpoch,
     TablerEpochRange,
     TablerBreadCrumb,
