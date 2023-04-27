05ab85d121e5fda2fee0f348ba1a7ffa74e72374
ingalls <nick@ingalls.ca>
2023-04-27 09:36:49 -0600

    Add Normalize Option

--- components/BreadCrumb.vue
+++ components/BreadCrumb.vue
@@ -10,7 +10,8 @@
         }'
     >
         <a v-if='crumb_it === crumbs.length - 1' v-text='crumb'></a>
-        <a v-else @click='$router.push("/" + crumbs.splice(0, crumb_it + 1).join("/").toLowerCase())' class='cursor-pointer' v-text='crumb'></a>
+        <a v-else-if='normalize' @click='$router.push("/" + crumbs.splice(0, crumb_it + 1).join("/").toLowerCase())' class='cursor-pointer' v-text='crumb'></a>
+        <a v-else @click='$router.push("/" + crumbs.splice(0, crumb_it + 1).join("/"))' class='cursor-pointer' v-text='crumb'></a>
     </li>
 </ol>
 </template>
@@ -18,12 +19,23 @@
 <script>
 export default {
     name: 'BreadCrumb',
+    props: {
+        normalize: {
+            type: Boolean,
+            default: true,
+            description: 'Perform Title Casing on URL Components'
+        }
+    },
     data: function() {
         return {
             crumbs: this.$route.path.split('/').filter((crumb) => {
                 return crumb.length;
             }).map((crumb) => {
-                return `${crumb[0].toUpperCase()}${crumb.slice(1, crumb.length)}`;
+                if (this.normalize) {
+                    return `${crumb[0].toUpperCase()}${crumb.slice(1, crumb.length)}`;
+                } else {
+                    return `${crumb[0]}${crumb.slice(1, crumb.length)}`;
+                }
             })
         }
     },
